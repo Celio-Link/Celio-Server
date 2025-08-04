5b453293b92dd8565d134300bb315775de4e4c36
Nils Kirchhof <nils.kirchhof@silab.de>
2025-08-04 19:20:01 +0200

    Change to ESnext

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import WebSocket, {WebSocketServer} from 'ws';
 import {Client} from "./client";
 import {ErrorType, ControlMessage, SessionCreationMessage, JoinMessage} from "./messages/session";
 import {Session} from "./session";
-const { nanoid } = await import('nanoid');
+import { nanoid } from 'nanoid'
 
 
 const wss = new WebSocketServer({ port: 8080 });
